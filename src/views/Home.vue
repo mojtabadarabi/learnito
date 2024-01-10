5bd9cdd93243786e279dcdc775ffd3837911e81e
Mojtaba darabi <darabi.dev@gmail.com>
2024-01-10 05:43:16 -0800

    some

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,39 @@
 <template>
     <div class="">
-        home
+        <Courses :courses="courses"/>
     </div>
 </template>
+
+<script lang="ts">
+import Courses from '../components/Courses/index.vue'
+export default {
+    name:"Home",
+    components: {
+        Courses
+    },
+    data(){
+        return {
+            courses:[
+                {
+                    title:'react ',
+                    description:'react is awesome ',
+                    price:210,
+                    isFavorite:false
+                },
+                {
+                    title:'vuejs ',
+                    description:'let me teach vuejs',
+                    price:450,
+                    isFavorite:false
+                },
+                {
+                    title:'angular',
+                    description:'hard but powerfull',
+                    price:740,
+                    isFavorite:false
+                },
+            ]
+        }
+    }
+}
+</script>
